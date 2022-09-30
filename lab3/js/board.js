75f2be3a181bc4062243f0cd144e5f04e294ed95
Mikhail Privalov <privalovmihail2@gmail.com>
2022-09-30 13:20:02 +0600

    .

--- lab3/js/board.js
+++ lab3/js/board.js
@@ -1,3 +1,10 @@
+const BOARD_SIZE = 8;
+
+const Point = {
+    x: Number,
+    y: Number
+}
+
 const MoveType = {
     MOVEMENT: 'green',
     ATTACK: 'red'
@@ -7,233 +14,176 @@ const PieceType = {
     WHITE: {style: 'white-piece', side: 'white'},
     BLACK: {style: 'black-piece', side: 'black'},
     WHITE_LADY: {style: 'white-lady', side: 'white'},
-    BLACK_LADY: {style: 'black-lady', side: 'black'},
+    BLACK_LADY: {style: 'black-lady', side: 'black'}
 }
 
 const Direction = {
-    UP_LEFT: {x: 1, y: -1},
+    UP_LEFT: {x: -1, y: 1},
     UP_RIGHT: {x: 1, y: 1},
     DOWN_LEFT: {x: -1, y: -1},
-    DOWN_RIGHT: {x: -1, y: 1}
+    DOWN_RIGHT: {x: 1, y: -1}
 }
 
-const boardLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
 const defaultPiecesLocations = [
-    { id: 'a1', type: PieceType.WHITE },
-    { id: 'c1', type: PieceType.WHITE },
-    { id: 'e1', type: PieceType.WHITE },
-    { id: 'g1', type: PieceType.WHITE },
-    { id: 'b2', type: PieceType.WHITE },
-    { id: 'd2', type: PieceType.WHITE },
-    { id: 'f2', type: PieceType.WHITE },
-    { id: 'h2', type: PieceType.WHITE },
-    { id: 'a3', type: PieceType.WHITE },
-    { id: 'c3', type: PieceType.WHITE },
-    { id: 'e3', type: PieceType.WHITE },
-    { id: 'g3', type: PieceType.WHITE },
-    { id: 'b8', type: PieceType.BLACK },
-    { id: 'd8', type: PieceType.BLACK },
-    { id: 'f8', type: PieceType.BLACK },
-    { id: 'h8', type: PieceType.BLACK },
-    { id: 'a7', type: PieceType.BLACK },
-    { id: 'c7', type: PieceType.BLACK },
-    { id: 'e7', type: PieceType.BLACK },
-    { id: 'g7', type: PieceType.BLACK },
-    { id: 'b6', type: PieceType.BLACK },
-    { id: 'd6', type: PieceType.BLACK },
-    { id: 'f6', type: PieceType.BLACK },
-    { id: 'h6', type: PieceType.BLACK },
+    { location: {x: 0, y: 0}, type: PieceType.WHITE },
+    { location: {x: 2, y: 0}, type: PieceType.WHITE },
+    { location: {x: 4, y: 0}, type: PieceType.WHITE },
+    { location: {x: 6, y: 0}, type: PieceType.WHITE },
+    { location: {x: 1, y: 1}, type: PieceType.WHITE },
+    { location: {x: 3, y: 1}, type: PieceType.WHITE },
+    { location: {x: 5, y: 1}, type: PieceType.WHITE },
+    { location: {x: 7, y: 1}, type: PieceType.WHITE },
+    { location: {x: 0, y: 2}, type: PieceType.WHITE },
+    { location: {x: 2, y: 2}, type: PieceType.WHITE },
+    { location: {x: 4, y: 2}, type: PieceType.WHITE },
+    { location: {x: 6, y: 2}, type: PieceType.WHITE },
+    { location: {x: 1, y: 7}, type: PieceType.BLACK },
+    { location: {x: 3, y: 7}, type: PieceType.BLACK },
+    { location: {x: 5, y: 7}, type: PieceType.BLACK },
+    { location: {x: 7, y: 7}, type: PieceType.BLACK },
+    { location: {x: 0, y: 6}, type: PieceType.BLACK },
+    { location: {x: 2, y: 6}, type: PieceType.BLACK },
+    { location: {x: 4, y: 6}, type: PieceType.BLACK },
+    { location: {x: 6, y: 6}, type: PieceType.BLACK },
+    { location: {x: 1, y: 5}, type: PieceType.BLACK },
+    { location: {x: 3, y: 5}, type: PieceType.BLACK },
+    { location: {x: 5, y: 5}, type: PieceType.BLACK },
+    { location: {x: 7, y: 5}, type: PieceType.BLACK },
 ]
 
 const example1PiecesLocations = [
-    { id: 'f4', type: PieceType.WHITE },
-    { id: 'h4', type: PieceType.WHITE },
-    { id: 'c1', type: PieceType.BLACK_LADY },
-    { id: 'b8', type: PieceType.BLACK },
-    { id: 'c5', type: PieceType.BLACK },
-    { id: 'c7', type: PieceType.BLACK },
-    { id: 'e7', type: PieceType.BLACK },
-    { id: 'h6', type: PieceType.BLACK },
+    { location: {x: 5, y: 3}, type: PieceType.WHITE },
+    { location: {x: 7, y: 3}, type: PieceType.WHITE },
+    { location: {x: 2, y: 0}, type: PieceType.BLACK_LADY },
+    { location: {x: 1, y: 7}, type: PieceType.BLACK },
+    { location: {x: 2, y: 4}, type: PieceType.BLACK },
+    { location: {x: 2, y: 6}, type: PieceType.BLACK },
+    { location: {x: 4, y: 6}, type: PieceType.BLACK },
+    { location: {x: 7, y: 5}, type: PieceType.BLACK },
 ]
 
 const example2PiecesLocations = [
-    { id: 'e3', type: PieceType.WHITE },
-    { id: 'd4', type: PieceType.BLACK },
-    { id: 'f4', type: PieceType.BLACK },
-    { id: 'b2', type: PieceType.WHITE },
+    { location: {x: 4, y: 2}, type: PieceType.WHITE },
+    { location: {x: 3, y: 3}, type: PieceType.BLACK },
+    { location: {x: 5, y: 3}, type: PieceType.BLACK },
+    { location: {x: 1, y: 1}, type: PieceType.WHITE },
 ];
 
 const cells = document.querySelectorAll("td");
 
-function findElementById(id) {
-    for (let i = 0; i < cells.length; i++) {
-        if (cells[i].id === id) {
-            return cells[i];
+function pointToId(point) {
+    return point.x.toString() + point.y.toString();
+}
+
+function idToPoint(id) {
+    return {
+        x: Number(id[0]),
+        y: Number(id[1]),
+    };
+}
+
+function pointsEqual(a, b) {
+    return a.x === b.x && a.y === b.y;
+}
+
+function findElementByPoint(point) {
+    let id = pointToId(point);
+
+    for (const cell of cells) {
+        if (cell.id === id) {
+            return cell
         }
     }
-
     return null;
 }
 
-function getPieceByIdDefaultBoard(id) {
-    for (let i = 0; i < defaultPiecesLocations.length; i++) {
-        if (id === defaultPiecesLocations[i].id) {
-            return defaultPiecesLocations[i].type;
-        }
-    }
-
-    return null;
+function getPieceByPointDefaultBoard(point) {
+    return defaultPiecesLocations.find(dpl => pointsEqual(dpl.location, point))?.type ?? null;
 }
 
-function getPieceByIdExample1Board(id) {
-    for (let i = 0; i < example1PiecesLocations.length; i++) {
-        if (id === example1PiecesLocations[i].id) {
-            return example1PiecesLocations[i].type;
-        }
-    }
-
-    return null;
+function getPieceByPointExample1Board(point) {
+    return example1PiecesLocations.find(dpl => pointsEqual(dpl.location, point))?.type ?? null;
 }
 
-function getPieceByIdExample2Board(id) {
-    for (let i = 0; i < example2PiecesLocations.length; i++) {
-        if (id === example2PiecesLocations[i].id) {
-            return example2PiecesLocations[i].type;
-        }
-    }
-
-    return null;
+function getPieceByPointExample2Board(point) {
+    return example2PiecesLocations.find(dpl => pointsEqual(dpl.location, point))?.type ?? null;
 }
 
 class Board {
-    cells = new Array(8);
+    cells = new Array(BOARD_SIZE);
     activeCell = {
-        id: String, 
+        location: Point,
         piece: PieceType | null,
         availableForMove: []
     } | null;
 
     constructor() {
         for (let i = 0; i < this.cells.length; i++) {
-            this.cells[i] = new Array(8);
+            this.cells[i] = new Array(BOARD_SIZE);
             for (let j = 0; j < this.cells[i].length; j++) {
-                let id = boardLetters[i] + (j + 1);
+                let location = {
+                    x: j,
+                    y: i
+                }
 
-                let element = findElementById(id);
+                let element = findElementByPoint(location);
                 element.addEventListener("click", () => this.handleBoardEvent(element));
 
                 this.cells[i][j] = {
-                    id: id,
-                    piece: getPieceByIdDefaultBoard(id),
+                    location: location,
+                    piece: getPieceByPointDefaultBoard(location),
                     availableForMove: []
                 }
             }
         }
 
         this.updateAvailableForMove();
-
         this.updateAllCellsView();
     }
 
     updateAvailableForMove() {
-        for (let i = 0; i < this.cells.length; i++) {
-            for (let j = 0; j < this.cells[i].length; j++) {
-                this.cells[i][j].availableForMove = this.getAvailableForMove(i, j, this.cells[i][j].piece);
+        for (let i = 0; i < BOARD_SIZE; i++) {
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                this.cells[i][j].availableForMove = this.getAvailableForMove(this.cells[i][j]);
             }
         }
     }
 
-    getAvailableForMove(x, y, targetPiece) {
+    getAvailableForMove(cell) {
         let availableForMove = [];
         let directions = [];
 
-        if (targetPiece == PieceType.WHITE) {
+        if (cell.piece === PieceType.WHITE) {
             directions.push(Direction.UP_LEFT, Direction.UP_RIGHT);
-        } else if (targetPiece == PieceType.BLACK) {
+        } else if (cell.piece === PieceType.BLACK) {
             directions.push(Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
-        } else if (!this.isEmptyCell(x, y)){
-            directions.push(Direction.UP_RIGHT, Direction.UP_LEFT, Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
+        } else if (cell.piece != null) {
+            directions.push(Direction.DOWN_LEFT, Direction.DOWN_RIGHT, Direction.UP_LEFT, Direction.UP_RIGHT);
         }
+
+        let location = cell.location;
 
         directions.forEach(direction => {
-            if (this.isOpponentPiece(x, y, x + direction.x, y + direction.y) && this.inBoard(x + 2 * direction.x, y + 2 * direction.y)) {               
+            if (this.isEmptyCell(location.x + direction.x, location.y + direction.y)) {
                 availableForMove.push({
-                    id: this.toBoardId(x + 2 * direction.x, y + 2 * direction.y),
-                    type: MoveType.ATTACK
-                });
-            } else if(this.inBoard(x + direction.x, y + direction.y)) {
-                availableForMove.push({
-                    id: this.toBoardId(x + direction.x, y + direction.y),
+                    location: {
+                        x: location.x + direction.x,
+                        y: location.y + direction.y
+                    },
                     type: MoveType.MOVEMENT
-                })
+                });
             }
         });
-
-        if (availableForMove.find(el => el.type == MoveType.ATTACK)) {
-            return availableForMove.filter(el => el.type == MoveType.ATTACK);
-        }
 
         return availableForMove;
     }
 
-    toDefaultState() {
-        this.toNormalMode();
-        for (let i = 0; i < this.cells.length; i++) {
-            for (let j = 0; j < this.cells[i].length; j++) {
-                let piece = getPieceByIdDefaultBoard(this.cells[i][j].id);
-
-                this.cells[i][j] = {
-                    id: this.cells[i][j].id,
-                    piece: piece,
-                    availableForMove: this.getAvailableForMove(i, j, piece)
-                }
-            }
-        }
-
-        this.updateAllCellsView();
-    }
-
-    toExample1State() {
-        this.toNormalMode();
-        for (let i = 0; i < this.cells.length; i++) {
-            for (let j = 0; j < this.cells[i].length; j++) {
-                let piece = getPieceByIdExample1Board(this.cells[i][j].id);
-
-                this.cells[i][j] = {
-                    id: this.cells[i][j].id,
-                    piece: piece,
-                    availableForMove: this.getAvailableForMove(i, j, piece)
-                }
-            }
-        }
-
-        this.updateAllCellsView();
-    }
-
-    toExample2State() {
-        this.toNormalMode();
-        for (let i = 0; i < this.cells.length; i++) {
-            for (let j = 0; j < this.cells[i].length; j++) {
-                let piece = getPieceByIdExample2Board(this.cells[i][j].id);
-
-                this.cells[i][j] = {
-                    id: this.cells[i][j].id,
-                    piece: piece,
-                    availableForMove: this.getAvailableForMove(i, j, piece)
-                }
-            }
-        }
-
-        this.updateAllCellsView();
-    }
-
     handleBoardEvent(selectedElement) {
-        if (this.activeCell && selectedElement.id === this.activeCell.id) {
+        if (this.activeCell && pointsEqual(idToPoint(selectedElement.id), this.activeCell.location)) {
             this.toNormalMode();
         } else {
             if (this.activeCell) {
-                endHighlight(this.activeCell.id);
+                endHighlight(this.activeCell.location);
                 this.endHighlightAvailableCells();
             }
 
@@ -241,44 +191,43 @@ class Board {
             this.activeCell = this.findById(selectedElement.id);
 
             if (this.activeCell.piece != null) {
-                this.highlightAvailableCells(selectedElement.id);
+                this.highlightAvailableCells();
             }
         }
     }
 
+    findById(id) {
+        let location = idToPoint(id);
+
+        for (let i = 0; i < BOARD_SIZE; i++) {
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                if (pointsEqual(this.cells[i][j].location, location)) {
+                    return this.cells[i][j];
+                }
+            }
+        }
+
+        return null;
+    }
+
     highlightAvailableCells() {
-        this.activeCell.availableForMove.forEach(el => highlightWithColor(el.id, el.type));
+        this.activeCell.availableForMove.forEach(el => highlightWithColor(el.location, el.type));
     }
 
     endHighlightAvailableCells() {
-        this.activeCell.availableForMove.forEach(el => endHighlight(el.id));
+        this.activeCell.availableForMove.forEach(el => endHighlight(el.location));
     }
 
     toNormalMode() {
         if (this.activeCell) {
-            endHighlight(this.activeCell.id);
+            endHighlight(this.activeCell.location);
             this.endHighlightAvailableCells();
             this.activeCell = null;
         }
     }
 
-    findById(id) {
-        let y = boardLetters.findIndex(el => el === id[0]);
-        let x = id[1] - 1;
-
-        return this.cells[y][x];
-    }
-
-    toBoardId(x, y) {
-        return boardLetters[y] + (x + 1);
-    }
-
     inBoard(x, y) {
-        return x >= 0 && x < 8 && y >= 0 && y < 8;
-    }
-
-    isOpponentPiece(x, y, otherX, otherY) {
-        return this.inBoard(otherX, otherY) && this.cells[y][x].piece?.side != this.cells[otherY][otherX].piece?.side
+        return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
     }
 
     isEmptyCell(x, y) {
@@ -295,15 +244,15 @@ class Board {
 }
 
 function updateView(cell) {
-    findElementById(cell.id).firstElementChild.className = cell.piece?.style;
+    findElementByPoint(cell.location).firstElementChild.className = cell.piece?.style;
 }
 
-function highlightWithColor(id, color) {
-    findElementById(id).style.backgroundColor = color;
+function highlightWithColor(location, color) {
+    findElementByPoint(location).style.backgroundColor = color;
 }
 
-function endHighlight(id) {
-    findElementById(id).style.removeProperty("background-color");
+function endHighlight(location) {
+    findElementByPoint(location).style.removeProperty("background-color");
 }
 
 let board = new Board();
